e3f1306cc27375ddabcf8ce61eb28472842a22c4
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-10-23 14:33:23 +0900

    set baseline

--- experiments/language_model/baseline.py
+++ experiments/language_model/baseline.py
@@ -12,8 +12,8 @@ def main():
     trainer.build()
     vocab_size = len(trainer.preprocessor.vocabulary.get())
     print("vocab size: {}".format(vocab_size))
-    model = LSTMLM(vocab_size, embedding_size=200, hidden_size=200, layers=2)
-    metrics = trainer.train(model, lr=15, decay=0.5, epochs=10)
+    model = LSTMLM(vocab_size)
+    trainer.train(model, epochs=10)
 
 
 if __name__ == "__main__":
